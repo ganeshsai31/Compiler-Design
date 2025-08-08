12f81e1e32b5acca91c376175cc686d46c17c302
ganeshsai31 <112364092+ganeshsai31@users.noreply.github.com>
2025-08-08 14:08:12 +0530

    Create 13.cpp

---
+++ 13.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *fp;
+    char filename[100], ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Could not open file %s\n", filename);
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        characters++;
+
+        if (ch == '\n')
+            lines++;
+
+        if (isspace(ch)) {
+            inWord = 0;
+        } else if (inWord == 0) {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("\n--- Lexical Analysis Summary ---\n");
+    printf("Characters: %d\n", characters);
+    printf("Words     : %d\n", words);
+    printf("Lines     : %d\n", lines);
+
+    return 0;
+}
